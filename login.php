d5fe0a92b57252a6020a93be7142148cb081706b
s1142407 <ronald.nederveen@student.nhlstenden.com>
2021-09-28 11:37:44 +0200

    toevoegen van pearcollege afbeelding

---
+++ login.php
@@ -0,0 +1,22 @@
+<?php
+include "header.php";
+?>
+
+<!DOCTYPE html>
+
+<body>
+
+
+
+    <div class="login">
+        <div class="loginHeader">
+            <img src="assets/img/PearCollege_logo.svg" alt="PearCollege Logo">
+            <h3></h3>
+        </div>
+
+
+
+    </div>
+</body>
+
+</html>
