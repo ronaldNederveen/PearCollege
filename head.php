0a6b54a5b0fe3a8146036a2422caae2ff57922f3
Sir-Sunshine <serhan.gunes@student.nhlstenden.com>
2021-09-28 12:41:57 +0200

    commends changed

--- head.php
+++ head.php
@@ -1,4 +1,4 @@
-
+    <!--img for the head -->
 <img src="img/PerCallege_logo.svg"    alt="logo_Pear" class="logo-head"> 
 <img src="icons/bell-solid.svg"       alt="bell"      class="bell-head"> 
 <img scr="icons/lock-solid.svg"       alt="lock"      class="lock-head">
@@ -6,7 +6,7 @@
 <img scr="icons/user-solid.svg"       alt="user"      class="user-head">
 <img scr="img/"                       alt="UK "       class="UK-head">
 
-    <!--text for the header-->
+    <!--text for the head-->
 <div class="title-head"> <p>Pear College</p> </div>
 <div class="translate-head"> <p>English</p>  </div>
 
