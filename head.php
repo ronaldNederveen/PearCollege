d15a2e8bccb00fa4611dd184ee02faccdcbacabc
Sir-Sunshine <serhan.gunes@student.nhlstenden.com>
2021-09-28 12:35:38 +0200

    van header class naar head

--- head.php
+++ head.php
@@ -1,16 +1,17 @@
-<img src="img/PerCallege_logo.svg"    alt="logo_Pear" class="logo-header"> 
-<img src="icons/bell-solid.svg"       alt="bell"      class="bell-header"> 
-<img scr="icons/lock-solid.svg"       alt="lock"      class="lock-header">
-<img scr="icons/search-solid.svg"     alt="search"    class="search-header">
-<img scr="icons/user-solid.svg"       alt="user"      class="user-header">
-<img scr="img/"                       alt="UK "       class="UK-header">
+
+<img src="img/PerCallege_logo.svg"    alt="logo_Pear" class="logo-head"> 
+<img src="icons/bell-solid.svg"       alt="bell"      class="bell-head"> 
+<img scr="icons/lock-solid.svg"       alt="lock"      class="lock-head">
+<img scr="icons/search-solid.svg"     alt="search"    class="search-head">
+<img scr="icons/user-solid.svg"       alt="user"      class="user-head">
+<img scr="img/"                       alt="UK "       class="UK-head">
 
     <!--text for the header-->
-<div class="title-header"> <p>Pear College</p> </div>
-<div class="translate-header"> <p>English</p>  </div>
+<div class="title-head"> <p>Pear College</p> </div>
+<div class="translate-head"> <p>English</p>  </div>
 
     <!--search bar -->
-<div class="searchbar-header">
+<div class="searchbar-head">
     <form>
         <imput type="text" id="searchbar" name="searchbar">
     </form>
