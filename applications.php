faf3710dd6c146750d0b4cd87b1530628ec7351a
Noizeisdead <noize829@gmail.com>
2021-09-29 11:10:42 +0200

    Added img tag applications

--- applications.php
+++ applications.php
@@ -2,23 +2,23 @@
 	<html lang="EN"> 
 		<head>
 		<meta charset="UTF-8"/> 
-		<link rel="stylesheet" href="Project_notes_applications.css"/>
+		<link rel="stylesheet" href="style.css"/>
 		</head>
 		<body>
 			<h1>Applications</h1>
 			<div id="application_box">
-				<div class="application"> <a href =""><img scr="img.jpg" alt="test img"/></a>1</div>
-				<div class="application"> 2</div>
-				<div class="application"> 3</div>
-				<div class="application"> 4</div>
-				<div class="application"> 5</div> 
-				<div class="application"> 6</div>
-				<div class="application"> 7</div>
-				<div class="application"> 8</div>
-				<div class="application"> 9</div>
-				<div class="application"> 10</div>
-				<div class="application"> 11</div>
-				<div class="application"> 12</div>
+				<div class="application"><a href =""><img scr="" alt=""/></a>1</div>
+				<div class="application"><a href =""><img scr="" alt=""/></a>2</div>
+				<div class="application"><a href =""><img scr="" alt=""/></a>3</div>
+				<div class="application"><a href =""><img scr="" alt=""/></a>4</div>
+				<div class="application"><a href =""><img scr="" alt=""/></a>5</div> 
+				<div class="application"><a href =""><img scr="" alt=""/></a>6</div>
+				<div class="application"><a href =""><img scr="" alt=""/></a>7</div>
+				<div class="application"><a href =""><img scr="" alt=""/></a>8</div>
+				<div class="application"><a href =""><img scr="" alt=""/></a>9</div>
+				<div class="application"><a href =""><img scr="" alt=""/></a>10</div>
+				<div class="application"><a href =""><img scr="" alt=""/></a>11</div>
+				<div class="application"><a href =""><img scr="" alt=""/></a>12</div>
 
 
         
