181d46656633a941cc6b85d6465afa348d745980
s1142407 <ronald.nederveen@student.nhlstenden.com>
2021-09-29 12:36:02 +0200

    vormgeven knoppen en invoervelden

--- login.php
+++ login.php
@@ -9,9 +9,20 @@ include "header.php";
             <h3>Pear College</h3>
         </div>
         <div class="loginInput">
+
+            <div class="emailField">
+                <img src="assets/icons/user-solid.svg" alt="Icon user" id="LoginUserIcon">
                 <input type="text" size="25" name="email" placeholder="E-mail..." id="emailInput">
+            </div>
+                
+                
                 <br>
+            <div class="passwordField">
+                <img src="assets/icons/lock-solid.svg" alt="Icon lock" id="LoginLockIcon">
                 <input type="password" size="25" name="Password" placeholder="Password..." id="passwordInput">
+            </div>
+        
+        
         </div>
         <div class="loginButton">
                 <input type="submit" name="login">    
