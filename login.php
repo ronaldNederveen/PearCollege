31e94eb09412df972470de347ae9467194938f0b
s1142407 <ronald.nederveen@student.nhlstenden.com>
2021-09-29 12:57:43 +0200

    fixed loginscreen

--- login.php
+++ login.php
@@ -14,34 +14,34 @@ include "header.php";
 
             <img src="assets/img/PearCollege_logo.svg" alt="PearCollege Logo" id="loginLogo" style="float:right" padding300px;>
         </div>
+
+
+        <h3>Pear College</h3>
+    
+    <div class="loginInput">
+
+        <div class="emailField">
+            <img src="assets/icons/user-solid.svg" alt="Icon user" id="LoginUserIcon">
+            <input type="text" size="25" name="email" placeholder="E-mail..." id="emailInput">
+        </div>
+
+
+
+        <br>
+        <div class="passwordField">
+            <img src="assets/icons/lock-solid.svg" alt="Icon lock" id="LoginLockIcon">
+            <input type="password" size="25" name="Password" placeholder="Password..." id="passwordInput">
+        </div>
+
+
+    </div>
+    <div class="loginButton">
+        <input type="submit" name="login">
     </div>
 
-    < <h3>Pear College</h3>
-        </div>
-        <div class="loginInput">
-
-            <div class="emailField">
-                <img src="assets/icons/user-solid.svg" alt="Icon user" id="LoginUserIcon">
-                <input type="text" size="25" name="email" placeholder="E-mail..." id="emailInput">
-            </div>
-
-            
-
-            <br>
-            <div class="passwordField">
-                <img src="assets/icons/lock-solid.svg" alt="Icon lock" id="LoginLockIcon">
-                <input type="password" size="25" name="Password" placeholder="Password..." id="passwordInput">
-            </div>
 
 
-        </div>
-        <div class="loginButton">
-            <input type="submit" name="login">
-        </div>
-
-
-
-        </div>
+    </div>
 </body>
 
 </html>
