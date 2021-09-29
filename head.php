8d07e1aa0528f6ca2fd7ec6c8567644c1a13db1a
Sir-Sunshine <serhan.gunes@student.nhlstenden.com>
2021-09-29 11:15:59 +0200

    give a logical order

--- head.php
+++ head.php
@@ -1,24 +1,27 @@
-
+ <?php
+  include "assets/css/style.css";
+ ?>
+ <!--Header of Pear College-->
+    <header>
+        <!--logo part -->
+        <div class="logo">
+            <img src="img/PerCallege_logo.svg"    alt="logo_Pear" class="logo-head">
+            <p class="title-head">Pear College</p>
+        </div>
+        <!--search bar part -->
+        <div class="search">
+            <img scr="icons/search-solid.svg"     alt="search"    class="search-head">
+                <form>
+                    <imput type="text" id="searchbar" name="searchbar">
+                </form>
+        </div>
+        <!--icons part-->
+        <div class="icons">
+            <img scr="img/"                       alt="UK"        class="UK-head">
+            <p class=translate-head>English</p> 
+            <img src="icons/bell-solid.svg"       alt="bell"      class="bell-head"> 
+            <img scr="icons/user-solid.svg"       alt="user"      class="user-head">
+        </div>    
+    </header> 
  
- <link rel="stylesheet" href="assets/css/style.css">
  
- <!--img for the head -->
-<img src="assets/img/PearCollege_logo.svg"    alt="logo_Pear" class="logo-head"> 
-<img src="assets/icons/bell-solid.svg"       alt="bell"      class="bell-head"> 
-<img scr="assets/icons/lock-solid.svg"       alt="lock"      class="lock-head">
-<img scr="assets/icons/search-solid.svg"     alt="search"    class="search-head">
-<img scr="assets/icons/user-solid.svg"       alt="user"      class="user-head">
-<img scr="assets/img/"                       alt="UK"        class="UK-head">
-
-    <!--text for the head-->
-<div class="title-head"> <p>Pear College</p> </div>
-<div class="translate-head"> <p>English</p>  </div>
-
-    <!--search bar -->
-<div class="searchbar-head">
-    <form>
-        <imput type="text" id="searchbar" name="searchbar">
-    </form>
-</div>
-
-</body>
