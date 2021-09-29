58328219d99aec08eade9e78867c15277e53a05c
s1142407 <ronald.nederveen@student.nhlstenden.com>
2021-09-29 12:52:55 +0200

    This reverts commit c5c8d27add9, reversing changes made to 47d66ce3a

--- login.php
+++ login.php
@@ -10,19 +10,13 @@ include "header.php";
 
     <div class="login">
         <div class="loginHeader">
-                
-                    
-                        <img src="assets/img/PearCollege_logo.svg" alt="PearCollege Logo" id="loginLogo" style="float:right" padding300px;> 
-</div>
-</div>
-
-        <
 
 
+            <img src="assets/img/PearCollege_logo.svg" alt="PearCollege Logo" id="loginLogo" style="float:right" padding300px;>
+        </div>
+    </div>
 
-
-
-            <h3>Pear College</h3>
+    < <h3>Pear College</h3>
         </div>
         <div class="loginInput">
 
@@ -30,23 +24,24 @@ include "header.php";
                 <img src="assets/icons/user-solid.svg" alt="Icon user" id="LoginUserIcon">
                 <input type="text" size="25" name="email" placeholder="E-mail..." id="emailInput">
             </div>
-                
-                
-                <br>
+
+            
+
+            <br>
             <div class="passwordField">
                 <img src="assets/icons/lock-solid.svg" alt="Icon lock" id="LoginLockIcon">
                 <input type="password" size="25" name="Password" placeholder="Password..." id="passwordInput">
             </div>
-        
-        
+
+
         </div>
         <div class="loginButton">
-                <input type="submit" name="login">    
+            <input type="submit" name="login">
         </div>
 
 
 
-    </div>
+        </div>
 </body>
 
 </html>
