d0f4f71ca1293341b76f4755d9218a8418862325
JaafarJawadi29 <91067539+JaafarJawadi29@users.noreply.github.com>
2021-09-29 11:03:47 +0200

    placement login page
    new class loginButton, changed margins in css

--- login.php
+++ login.php
@@ -3,26 +3,19 @@ include "header.php";
 ?>
 
 <body class="loginScreen">
-
-
-
     <div class="login">
         <div class="loginHeader">
             <img src="assets/img/PearCollege_logo.svg" alt="PearCollege Logo" id="loginLogo">
             <h3>Pear College</h3>
         </div>
         <div class="loginInput">
-            <form>
-                <input type="text" name="email" placeholder="E-mail..." id="emailInput">
-                <input type="password" name="Password" placeholder="Password..." id="passwordInput">
-
-                <input type="submit" name="login">
-            </form>
-
+                <input type="text" size="25" name="email" placeholder="E-mail..." id="emailInput">
+                <br>
+                <input type="password" size="25" name="Password" placeholder="Password..." id="passwordInput">
         </div>
-
-
-
+        <div class="loginButton">
+                <input type="submit" name="login">    
+        </div>
     </div>
 </body>
 
