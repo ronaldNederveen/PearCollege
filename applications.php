106654b63400170addd603dee55cdbae2c6e750c
Noizeisdead <noize829@gmail.com>
2021-09-30 09:33:33 +0200

    applications + style problem

--- applications.php
+++ applications.php
@@ -3,22 +3,25 @@
 		<head>
 		<meta charset="UTF-8"/> 
 		<link rel="stylesheet" href="assets/css/style.css"/>
+		<style> 
+			.app {
+				background-color: red;
+			}
+		</style>
 		</head>
 		<body>
-			<div id="app_box">
 				<div id="title_applications"><h3>Applications</h3></div>
-				<div id="applications_box"> 	
-					<div class="application"><a href =""><img scr="" alt=""/></a>1</div>
-					<div class="application"><a href =""><img scr="" alt=""/></a>2</div>
-					<div class="application"><a href =""><img scr="" alt=""/></a>3</div>
-					<div class="application"><a href =""><img scr="" alt=""/></a>4</div>
-					<div class="application"><a href =""><img scr="" alt=""/></a>5</div> 
-					<div class="application"><a href =""><img scr="" alt=""/></a>6</div>
-					<div class="application"><a href =""><img scr="" alt=""/></a>7</div>
-					<div class="application"><a href =""><img scr="" alt=""/></a>8</div>
-					<div class="application"><a href =""><img scr="" alt=""/></a>9</div>
-					<div class="application"><a href =""><img scr="" alt=""/></a>10</div>
+				<div id="app_box"> 	
+					<div class="app"><a href =""><img scr="" alt=""/></a>1</div>
+					<div class="app"><a href =""><img scr="" alt=""/></a>2</div>
+					<div class="app"><a href =""><img scr="" alt=""/></a>3</div>
+					<div class="app"><a href =""><img scr="" alt=""/></a>4</div>
+					<div class="app"><a href =""><img scr="" alt=""/></a>5</div> 
+					<div class="app"><a href =""><img scr="" alt=""/></a>6</div>
+					<div class="app"><a href =""><img scr="" alt=""/></a>7</div>
+					<div class="app"><a href =""><img scr="" alt=""/></a>8</div>
+					<div class="app"><a href =""><img scr="" alt=""/></a>9</div>
+					<div class="app"><a href =""><img scr="" alt=""/></a>10</div>
 				</div>
-			</div>
-		</body> 
+		</body>
 	</html>
