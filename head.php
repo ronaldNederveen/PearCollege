20ef1879a9ed23b5624518f58cbbdb6bae44e948
Sarah Vogelzang <sarah.vogelzang@student.nhlstenden.com>
2021-09-30 11:12:36 +0200

    again

--- head.php
+++ head.php
@@ -18,7 +18,7 @@
         <!--icons part-->
         <div class="icons">
             <img src="assets/icons/united-kingdom.svg"                       alt="UK"        class="UK-head">
-            <img src="assets/icons/united-kingdom.svg"   alt="UK"        class="UK-head">
+         
             <p class=translate-head>English</p> 
             <img src="assets/icons/bell-solid.svg"       alt="bell"      class="bell-head"> 
             <img src="assets/icons/user-solid.svg"       alt="user"      class="user-head">
