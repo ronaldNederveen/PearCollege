bd11bee6d9c3fa4e204a29d9329bfbe1ab48f80e
Noizeisdead <noize829@gmail.com>
2021-09-30 20:57:22 +0200

    Worksbystudents saving progress

---
+++ workbystudents.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+	<html lang="EN"> 
+		<head>
+		<meta charset="UTF-8"/> 
+		<link rel="stylesheet" href="assets/css/style.css"/>
+		</head>
+		<body>
+            <div> 
+                <h1>Works made by students</h1>
+                <div id="studentworks"> 
+                    <div class="studentapp"></div>
+                    <div class="studentapp"></div>
+                    <div class="studentapp"></div>
+                    <div class="studentapp"></div>
+                </div>
+            </div>
+		</body>
+	</html>
