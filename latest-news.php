2a2dc37f0836dae142e50c813a296b519b4ef541
Sir-Sunshine <serhan.gunes@student.nhlstenden.com>
2021-10-04 10:49:19 +0200

    add "" for the last time

--- latest-news.php
+++ latest-news.php
@@ -2,14 +2,14 @@
   include "header.php";
  ?>
 <!-- title of latest news -->
- <p class=title-news>Latest News</p>
+ <p class="title-news">Latest News</p>
 
 <!-- news blocks -->
- <div class=latest-news>
+ <div class="latest-news">
      <div class="ln1"> 
-         <img src="example.PNG" alt=news class="imgln">  <!-- img for the latest news -->
-         <p class="lntext">Lorum Ipsum</p>    <!-- summary of the news -->
-         <a class="link-news" href="" Read more >             <!-- link to the news -->
+         <img src="example.PNG" alt="news" class="imgln">   <!-- img for the latest news -->
+         <p class="lntext">Lorum Ipsum</p>                  <!-- summary of the news -->
+         <a class="link-news" href="" Read more >           <!-- link to the news -->
          </div>
      <div class="ln2"> 
          <img src="example.PNG" alt="news" class="imgln">
@@ -31,7 +31,7 @@
          <p class="lntext">Lorum Ipsum</p>
          <a href="" Read more>
          </div>     
-     <div class=ln6> 
+     <div class="ln6"> 
          <img src="example.PNG" alt="news" class="imgln">
          <p class="lntext">Lorum Ipsum</p>
          <a href="" Read more>
