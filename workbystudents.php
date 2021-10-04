42a94417b5c346c621a3609c8db730bcb088199e
s1142407 <ronald.nederveen@student.nhlstenden.com>
2021-10-04 12:01:51 +0200

    toevoegen van json lezen

--- workbystudents.php
+++ workbystudents.php
@@ -1,18 +1,49 @@
 <!DOCTYPE html>
-	<html lang="EN"> 
-		<head>
-		<meta charset="UTF-8"/> 
-		<link rel="stylesheet" href="assets/css/style.css"/>
-		</head>
-		<body>
-            <div> 
-                <h1>Works made by students</h1>
-                <div id="studentworks"> 
-                    <div class="studentapp"></div>
-                    <div class="studentapp"></div>
-                    <div class="studentapp"></div>
-                    <div class="studentapp"></div>
-                </div>
-            </div>
-		</body>
-	</html>
+<html lang="EN">
+
+<head>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" href="assets/css/style.css" />
+</head>
+
+<body>
+    <div>
+        <h1>Works made by students</h1>
+        <div id="studentworks">
+            <div class="studentapp"></div>
+            <div class="studentapp"></div>
+            <div class="studentapp"></div>
+            <div class="studentapp"></div>
+        </div>
+    </div>
+</body>
+
+</html>
+
+<?php
+$data = file_get_contents("workByStudents.json");
+$data = json_decode($data);
+//var_dump($data);
+    foreach ($data as $key => $value) {
+        foreach ($value as $key => $waarde) {
+            echo $key.": ".$waarde;
+            echo "</br>";
+        }
+
+        echo $value["releaseDate"];
+        //print_r($value);
+        echo "</br>";
+    }
+
+
+function StudentWork($work){
+    echo "<div class=\"studentapp\">";
+
+
+
+    echo "</div>";
+
+
+}
+
+?>
