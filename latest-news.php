c06ae48e3c54d8947d1b507c0ac7a0e9f52feb2d
Sir-Sunshine <serhan.gunes@student.nhlstenden.com>
2021-10-04 10:05:25 +0200

    Latest news

---
+++ latest-news.php
@@ -0,0 +1,17 @@
+<?php
+  include "header.php";
+ ?>
+
+ <p class=title-news>Latest News</p>
+
+ <div class=latest-news>
+     <div class=ln1> 
+         <img  src="" class= >
+         <p class=lntext>Lorum Ipsum</p>
+         <a href="" Read more >
+         </div>
+     <div class=ln2> <p class=lntext>Lorum Ipsum</p> </div>
+     <div class=ln3> <p class=lntext>Lorum Ipsum</p> </div>
+     <div class=ln4> <p class=lntext>Lorum Ipsum</p> </div>
+     <div class=ln5> <p class=lntext>Lorum Ipsum</p> </div>
+</div>
