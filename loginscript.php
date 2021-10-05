1b29bd33a58bad68cfd540e69944d9197ba0eff6
JaafarJawadi29 <91067539+JaafarJawadi29@users.noreply.github.com>
2021-10-05 12:41:36 +0200

    changed some things in loginscript.php, login.php and index.php

--- loginscript.php
+++ loginscript.php
@@ -1,19 +1,23 @@
               
-        <?php  session_start();
-    if(isset($_POST['Submit'])){
-    $logins = array('AchmedJ2002' => 'Achmed1234');
+        <?php 
+        session_start();
+        $users_json = file_get_contents('user.json');
+        $data = json_decode($users_json, true);
+        var_dump($data);
+        if (isset($_POST['Submit'])) {
+            $logins = array('email' => 'Password');
 
-    $Username = isset($_POST['AchmedJ2002']) ? $_POST['AchmedJ2002'] : '';
-$Password = isset($_POST['Achmed1234']) ? $_POST['Achmed1234'] : '';
+            $gebruikersnaam = isset($_POST['email']) ? $_POST['email'] : '';
+            $wachtwoord = isset($_POST['Password']) ? $_POST['Password'] : '';
 
-if (isset($logins[$Username]) && $logins[$Username] == $Password){
-$_SESSION['UserData']['Username']=$logins[$Username];
-header("location:index.php");
-exit;
-} else {
-$msg="Geen geldige inlogpoging";
-}
-}
-?>
+            if (isset($logins[$gebruikersnaam]) && $logins[$gebruikersnaam] == $wachtwoord) {
+                $_SESSION['UserData']['email'] = $logins[$gebruikersnaam];
+                header("location:index.php");
+                exit;
+            } else {
+                $msg = "Geen geldige inlogpoging";
+            }
+        }
+        ?>
    
    
