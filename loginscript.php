32b5907bc117743df2ef625c4fd985913d639f14
JaafarJawadi29 <91067539+JaafarJawadi29@users.noreply.github.com>
2021-10-05 12:00:58 +0200

    username and password changed

--- loginscript.php
+++ loginscript.php
@@ -3,8 +3,8 @@
     if(isset($_POST['Submit'])){
     $logins = array('AchmedJ2002' => 'Achmed1234');
 
-    $Username = isset($_POST['Username']) ? $_POST['Username'] : '';
-$Password = isset($_POST['Password']) ? $_POST['Password'] : '';
+    $Username = isset($_POST['AchmedJ2002']) ? $_POST['AchmedJ2002'] : '';
+$Password = isset($_POST['Achmed1234']) ? $_POST['Achmed1234'] : '';
 
 if (isset($logins[$Username]) && $logins[$Username] == $Password){
 $_SESSION['UserData']['Username']=$logins[$Username];
