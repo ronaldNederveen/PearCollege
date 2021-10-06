66be61fcd4f3db114791de819a57468c9ac7880f
Sarah Vogelzang <sarah.vogelzang@student.nhlstenden.com>
2021-10-06 10:36:48 +0200

    quick fix after merge

--- loginscript.php
+++ loginscript.php
@@ -5,16 +5,12 @@
         $data = json_decode($users_json, true);
         //var_dump($data);
         if (isset($_POST['Submit'])) {
-<<<<<<< HEAD
            
-=======
-            $logins = array('username' => 'Password');
->>>>>>> a53283becbf6383ab8457fe4cfc4313efa2c03e2
+            //$logins = array('username' => 'Password');
 
             $gebruikersnaam = isset($_POST['username']) ? $_POST['username'] : '';
             $wachtwoord = isset($_POST['Password']) ? $_POST['Password'] : '';
 
-<<<<<<< HEAD
             $user = findOne(array('gebruikersnaam' => 'user1', 'wachtwoord' => 'pass1'));
 
 
@@ -24,24 +20,14 @@
                 } else{
                     echo "Geen geldige inlogpoging";
                 }
-=======
             if (isset($logins[$gebruikersnaam]) && $logins[$gebruikersnaam] == $wachtwoord) {
                 $_SESSION['UserData']['username'] = $logins[$gebruikersnaam];
                 header("location:index.php");
                 exit;
             } else {
                 $msg = "Geen geldige inlogpoging";
->>>>>>> a53283becbf6383ab8457fe4cfc4313efa2c03e2
             }
-
-//          if (isset($gebruikersnaam) == 'user1' && isset($wachtwoord) == 'pass1') {
-//                $_SESSION['UserData']['email'] = $logins[$gebruikersnaam];
-  //              header("location:index.php");
-    //            exit;
-      //      } else {
-        //        $msg = "Geen geldige inlogpoging";
-          //  }
-        
+          }
 
         ?>
    
