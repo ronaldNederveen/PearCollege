21e5b36f8bb967d522eb27aef0993a638a7eff83
Noizeisdead <noize829@gmail.com>
2021-10-07 09:49:27 +0200

    Moved text in app_box

--- workbystudents.php
+++ workbystudents.php
@@ -42,9 +42,10 @@
         echo "<img src=\"assets/img/workByStudents/".$value['image']."\">";
         echo "<div id=\"text\">";
         echo "<p class=\"description\">".$value['description']."</p>";
+        echo "<div id=\"q\">";
         echo "<p class=\"creator\">".$value['creator']."</p>";
         echo "<p class=\"releaseDate\">".$value['releaseDate']."</p>";
-        echo "</div></div></div>";
+        echo "</div></div></div></div>";
     }
 
     ?>
