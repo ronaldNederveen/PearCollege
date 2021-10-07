539312d5e3a742b82f19e8ff25efd48855e37fbd
ItzJoris03 <jorishummel03@gmail.com>
2021-10-07 11:34:06 +0200

    hero.php fix

--- index.php
+++ index.php
@@ -14,27 +14,9 @@ if(!isset($_SESSION['username'])){
     header("location:login.php");
     exit;
 }
-?>
-<!-- Welcome student -->
-
-    <div class="hero">
-        <div class="intro">
-            <h1>Welcome Joris</h1>
-            <div class="box"></div>
-            <i class="corner"></i>
-            <i class="top"></i>
-        </div>
-        
-        <!--Boxes for all news and apps -->
-        <div class="news"></div>
-        <div class="apps">
-        </div>
-    </div>  
-
-<?php
+include "hero.php";
 
 include "popups.php";
-include "latest-news.php";
 include "foot.php";
 
 ?>
