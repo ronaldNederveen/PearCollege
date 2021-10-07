bb4406d51d5cbfa2715e91b1264f9e4d1c0b8e1c
s1142407 <ronald.nederveen@student.nhlstenden.com>
2021-10-07 13:38:12 +0200

    added icon in header

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="icon" href="assets/img/pearCollege_logo.png">
     <title>PearCollege</title>
     <script src="assets/js/popup-user.js"></script>
 </head>
