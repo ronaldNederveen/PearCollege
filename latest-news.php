18e7c3138164582741a94f5b26059ca135ceb1c4
Noizeisdead <noize829@gmail.com>
2021-10-13 10:54:12 +0200

    Cleaned up unnecessary html

--- latest-news.php
+++ latest-news.php
@@ -1,61 +1,7 @@
 <?php
   include "header.php";
- ?>
- 
-<div class="latest-block">
-<!-- title of latest news -->
- <p class="title-news">Latest News</p>
 
-
-<!-- news blocks -->
- <div class="latest-news">
-     <div class="ln"> 
-         <img src="assets/img/news-img/example.PNG" alt="news" class="imgln">   <!-- img for the latest news -->
-            <div class="text-link">
-                <p class="lntext">Lorum Ipsum</p>                                      <!-- summary of the news -->
-                <a class="link-news" href="" >Read more</a>                            <!-- link to the news -->
-            </div>
-         </div>
-     <div class="ln"> 
-         <img src="assets/img/news-img/example.PNG" alt="news" class="imgln">
-            <div class="text-link">
-                <p class="lntext">Lorum Ipsum</p>                                      <!-- summary of the news -->
-                <a class="link-news" href="" >Read more</a>                            <!-- link to the news -->
-            </div>   
-         </div>
-     <div class="ln"> 
-         <img src="assets/img/news-img/example.PNG" alt="news" class="imgln">
-            <div class="text-link">
-                <p class="lntext">Lorum Ipsum</p>                                      <!-- summary of the news -->
-                <a class="link-news" href="" >Read more</a>                            <!-- link to the news -->
-            </div>  
-         </div>
-     <div class="ln"> 
-         <img src="assets/img/news-img/example.PNG" alt="news" class="imgln">
-            <div class="text-link">
-                <p class="lntext">Lorum Ipsum</p>                                      <!-- summary of the news -->
-                <a class="link-news" href="" >Read more</a>                            <!-- link to the news -->
-            </div>
-         </div>
-     <div class="ln"> 
-         <img src="assets/img/news-img/example.PNG" alt="news" class="imgln">
-            <div class="text-link">
-                <p class="lntext">Lorum Ipsum</p>                                      <!-- summary of the news -->
-                <a class="link-news" href="" >Read more</a>                            <!-- link to the news -->
-            </div>  
-         </div>     
-     <div class="ln"> 
-         <img src="assets/img/news-img/example.PNG" alt="news" class="imgln">
-            <div class="text-link">
-                <p class="lntext">Lorum Ipsum</p>                                      <!-- summary of the news -->
-                <a class="link-news" href="" >Read more</a>                            <!-- link to the news -->
-            </div>  
-         </div>    
-    </div>
-</div>
-
-
-<?php 
+  
 //Loading in the JSON file
 $news = file_get_contents("latest-news.json");
 //JSON data converting to PHP Array
@@ -67,24 +13,31 @@ $news = json_decode($news, true);
     function NewsArticle($value){
         
         echo "<div class=\"ln\"> ";
-        echo "<img src=\"".$value['image']."\" alt=\"news\" class=\"imgln\"/>";
+        echo "<img src=\"".$value['image']."\" alt=\"news\" class=\"imgln\"/>"; #Image for the news 
         echo "<div class=\"text-link\">";
-        echo "<p class=\"lntext\">".$value[title]."</p>";
-        echo "<a class=\"link-news\" href=\"".$value[link]."\" >Read more</a>";
+        echo "<p class=\"lntext\">".$value[title]."</p>"; #Title for the news 
+        echo "<a class=\"link-news\" href=\"".$value[link]."\" >Read more</a>"; #link to the news 
         echo "</div></div>";
     }
 ?>
 
-<?php 
-    echo "<div class=\"latest-block\">";
-    echo "<p class=\"title-news\">Latest News</p>";
+<div class="latest-block">
+<!-- title of latest news -->
+ <p class="title-news">Latest News</p>
 
 
+ <?php 
+   
     echo "<div class=\"latest-news\">";
     //Loop for all the JSON objects / different news articles 
     foreach($news as $news_article){
         NewsArticle($news_article);
     }
-
-    echo "</div></div>";
 ?>
+    
+    </div>
+</div>
+
+
+
+
