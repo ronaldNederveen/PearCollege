4aa10986ccb9a6b4c56998104a9d81520bf2d5b6
Noizeisdead <noize829@gmail.com>
2021-10-13 10:40:18 +0200

    Added comments

--- latest-news.php
+++ latest-news.php
@@ -56,20 +56,21 @@
 
 
 <?php 
-echo "<br><br>";
-echo "<br><br>";
-
+//Loading in the JSON file
 $news = file_get_contents("latest-news.json");
+//JSON data converting to PHP Array
+$news = json_decode($news, true);
 
-    $news = json_decode($news, true);
-
+    //Function to print ever news article block 
+    //Input: Array with information about the news article
+    //Output: News article block
     function NewsArticle($value){
         
         echo "<div class=\"ln\"> ";
         echo "<img src=\"".$value['image']."\" alt=\"news\" class=\"imgln\"/>";
         echo "<div class=\"text-link\">";
-        echo "<p class=\"lntext\">Lorum Ipsum</p>";
-        echo "<a class=\"link-news\" href=\"\" >Read more</a>";
+        echo "<p class=\"lntext\">".$value[title]."</p>";
+        echo "<a class=\"link-news\" href=\"".$value[link]."\" >Read more</a>";
         echo "</div></div>";
     }
 ?>
@@ -80,8 +81,10 @@ $news = file_get_contents("latest-news.json");
 
 
     echo "<div class=\"latest-news\">";
-
+    //Loop for all the JSON objects / different news articles 
     foreach($news as $news_article){
         NewsArticle($news_article);
     }
+
+    echo "</div></div>";
 ?>
