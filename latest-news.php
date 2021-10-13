91a523125d93a092df0bc1dec0cb948e65a8e930
Noizeisdead <noize829@gmail.com>
2021-10-13 09:47:24 +0200

    Started conerting HTML to php for JSON file

--- latest-news.php
+++ latest-news.php
@@ -53,3 +53,31 @@
          </div>    
     </div>
 </div>
+
+
+<?php 
+    $news = file_get_contents("latest-news.json");
+
+    $news = json_decode($news, true);
+
+    function NewsArticle($value){
+        echo "<div class=\"ln\"> ";
+        echo "<img src=\"assets/img/news-img/\"latest-news-covid.jpeg\" alt=\"news\" class=\"imgln\"/>";
+        echo "<div class=\"text-link\">";
+        echo "<p class=\"lntext\">Lorum Ipsum</p>";
+        echo "<a class=\"link-news\" href=\"\" >Read more</a>";
+        echo "</div></div>";
+    }
+?>
+
+<?php 
+    echo "<div class=\"latest-block\">";
+    echo "<p class=\"title-news\">Latest News</p>";
+
+
+    echo "<div class=\"latest-news\">";
+
+    foreach($news as $news_article){
+        NewsArticle($news_article);
+    }
+?>
