867c5150643b44d611a3a2d24cf8c795f3389896
JonathanHeezen <91133570+JonathanHeezen@users.noreply.github.com>
2021-10-14 11:44:39 +0200

    footer verbetering

--- Foot.php
+++ Foot.php
@@ -2,9 +2,9 @@
     include "header.php";
 ?>
     <!--Footer of Pear College (2 boxes te get the text to the center)-->
-        <div class="footersmall"></div>
-        <div class="footerbig">
-        
+        <div class="footer">
+    
+
         <!--Footer text-->
             <p class="copyright-notice"> &copy;2021|Pear College </p> 
         </div>
