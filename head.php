636d82bb30e3afdc412c0840fdf38a2e8ec4b5bc
ItzJoris03 <jorishummel03@gmail.com>
2021-10-14 11:05:24 +0200

    Added notifications popup toggle function to bell

--- head.php
+++ head.php
@@ -21,7 +21,7 @@
                 <img src="assets/icons/united-kingdom.svg"   alt="UK"        class="UK-head">
                 <p class=translate-head>English</p> 
             </div>
-            <i class="btn fas fa-bell" id="popupBell"></i>
+            <i class="btn fas fa-bell" id="popupBell" onclick="toggleNotificationsPopup()"></i>
             <i class="btn fas fa-user" id="popupUser" onclick="toggleUserPopup()"></i>
         </div>    
     </header> 
