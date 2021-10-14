0b57240fa525267e4a8567900fbbabd94c62756b
Sarah Vogelzang <sarah.vogelzang@student.nhlstenden.com>
2021-10-14 11:20:15 +0200

    kut zooi

--- assets/inc/login.inc.php
+++ assets/inc/login.inc.php
@@ -10,14 +10,31 @@ $json_data = json_decode($json, true);
           
 
 if (isset($_POST['username']) && !isset($_SESSION['username'])) {
+   
     foreach($json_data as $student){
+        
             //var_dump($student["gebruikersnaam"]);
             //echo "volgende item";
             if($_POST['username'] == $student["gebruikersnaam"]){
+               //var_dump($_POST['username']);
+               //var_dump($student['gebruikersnaam']);
 
+                
+                if($_POST['password'] == $student["wachtwoord"]){
+                    //var_dump($_POST['password']);
+               //var_dump($student["wachtwoord"]);
+
+                    
+                    $_SESSION['username'] = $_POST['username'];
+
+                }
+                            //var_dump($_SESSION['username']);
+                
             }
 }
-        //exit;
+       // exit;
+       ob_start();
+       session_start();
 
 
 
