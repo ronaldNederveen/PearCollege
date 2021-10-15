a56f4cce095b88c978b6c6623364cbeb4882d8d6
JaafarJawadi29 <91067539+JaafarJawadi29@users.noreply.github.com>
2021-10-15 12:56:28 +0200

    added pictures to applications.php

--- applications.php
+++ applications.php
@@ -1,12 +1,12 @@
 		<div class="applications">
 			<div id="title_applications"><h2>Applications</h2></div>
 			<div id="app_box"> 	
-				<div class="app"><a href =""><img scr="" alt=""/></a>1</div>
-				<div class="app"><a href =""><img scr="" alt=""/></a>2</div>
-				<div class="app"><a href =""><img scr="" alt=""/></a>3</div>
-				<div class="app"><a href =""><img scr="" alt=""/></a>4</div>
-				<div class="app"><a href =""><img scr="" alt=""/></a>5</div> 
-				<div class="app"><a href =""><img scr="" alt=""/></a>6</div>
+				<div class="app"><a href ="https://office365.com/"><img scr="assets/img/office2.png" alt="Office"/></a></div>
+				<div class="app"><a href ="https://icloud.com/"><img scr="assets/img/icloud.png" alt="iCloud"/></a></div>
+				<div class="app"><a href ="https://developer.apple.com/"><img scr="assets/img/appledeveloper.png" alt="Apple Developer"/></a></div>
+				<div class="app"><a href ="https://appstoreconnect.apple.com/"><img scr="assets/img/appstore.png" alt="App Store"/></a></div>
+				<div class="app"><a href ="https://developer.apple.com/forums/"><img scr="assets/img/appleforums.png" alt="Apple Forums"/></a></div> 
+				<div class="app"><a href ="https://www.apple.com/nl/newsroom/"><img scr="assets/img/applenewsroom.png" alt="Apple Newsroom"/></a></div>
 				<div class="app"><a href =""><img scr="" alt=""/></a>7</div>
 				<div class="app"><a href =""><img scr="" alt=""/></a>8</div>
 				<div class="app"><a href =""><img scr="" alt=""/></a>9</div>
