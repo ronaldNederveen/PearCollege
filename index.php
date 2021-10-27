7677ada9cf5f1e29fa638b4168c577f4b0dee48a
Noizeisdead <noize829@gmail.com>
2021-10-27 13:26:28 +0200

    Rss feed edit

--- index.php
+++ index.php
@@ -6,16 +6,20 @@
 * Programmer('s):
 *
 -->
-<?php session_start();
+<?php 
+session_start();
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 include "header.php";
 include "head.php";
 include "hero.php";
 include "applications.php";
 include "workbystudents.php";
-include "rssFeed.php";
+require "rssFeed.php";
 
 include "popups.php";
-include "foot.php";
+include "Foot.php";
 
 ?>
