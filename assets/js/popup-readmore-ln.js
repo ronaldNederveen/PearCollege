fdd776845ded9177ac77c6b8f0fe8d05d3c2b7f6
ItzJoris03 <jorishummel03@gmail.com>
2021-10-27 12:10:06 +0200

    popups read more - only styling needed

--- assets/js/popup-readmore-ln.js
+++ assets/js/popup-readmore-ln.js
@@ -8,4 +8,25 @@ function toggleReadMore(value) {
     popupln.classList.toggle('dis');
 
     checkState();
+
+    if(!popupln.classList.contains('dis')) {
+        lnimg = document.getElementById('pp-lnimg');
+        lntitle = document.getElementById('pp-lntitle');
+        lntext = document.getElementById('pp-lntext');
+
+        // Replace ./data.json with your JSON feed
+        fetch('./latest-news.json').then(response => {
+            return response.json();
+        }).then(data => {
+            // Work with JSON data here
+            items = Object.keys(data);
+            items.forEach(item => {
+                if(data[item]['id'] == value) {
+                    lnimg.src = data[item]['image'];
+                    lntitle.innerHTML = data[item]['title'];
+                    lntext.innerHTML = data[item]['text'];
+                }
+            })
+        });
+    }
 }
